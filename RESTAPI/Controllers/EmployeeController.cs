f7c5959ef5d75e3767cf213d7f318fac665200b8
Your Name <you@example.com>
2021-04-12 11:23:37 +0300

    First project

---
+++ RESTAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RESTAPI.Data;
+using RESTAPI.Services;
+
+namespace RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        EmployeeServiceDapper empService = new EmployeeServiceDapper();
+        public async Task<IEnumerable<Employeeinfo>> GetEmployee()
+        {
+            return await empService.GetEmployee();
+        }
+        [HttpGet]
+        [Route("GetById/{id}")]
+        public Employeeinfo GetEmployeeByID(int id)
+        {
+            return empService.GetEmployeeByID(id);
+        }
+        [HttpPost]
+        public void AddEmployee(Employeeinfo ecAdd)
+        {
+            empService.AddEmployee(ecAdd);
+        }
+        [HttpPut]
+        public void UpdateEmp(Employeeinfo Emp)
+        {
+            empService.UpdateEmp(Emp);
+        }
+        [HttpDelete]
+        [Route("DeleteById/{id}")]
+        public void DeleteEmp(int id)
+        {
+            empService.DeleteEmp(id);
+        }
+    }
+}
---
+++ RESTAPI/Services/EmployeeServiceDapper.cs
@@ -0,0 +1,82 @@
+﻿
+using Dapper;
+using RESTAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTAPI.Services
+{
+    public class EmployeeServiceDapper
+    {
+        private string connectionString = "";
+        public EmployeeServiceDapper()
+        {
+            connectionString = @"Data Source=Ghada-PC;Initial Catalog=Exam001;Integrated Security=True";
+        }
+        public IDbConnection connection
+        {
+            get { return new SqlConnection(connectionString); }
+        }
+        public void AddEmployee(Employeeinfo ecAdd)
+        {
+            using (IDbConnection dbConnection = connection)
+            {
+                string sQuery = @"INSERT INTO EMPTABLE(EmpName,Department,Salary)VALUES(@EmpName,@Department,@Salary)";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, ecAdd);
+
+            }
+        }
+        public async Task<IEnumerable<Employeeinfo>> GetEmployee()
+        {
+            try
+            {
+                using (IDbConnection dbConnection = connection)
+                {
+                    dbConnection.Open();
+                    return await dbConnection.QueryAsync<Employeeinfo>("SELECT * FROM Emptable");
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+          
+        }
+        public Employeeinfo GetEmployeeByID(int id)
+        {
+            using (IDbConnection dbConnection = connection)
+            {
+                string sQuery = @"SELECT * FROM Emptable WHERE EmpId=@id";
+                dbConnection.Open();
+                return dbConnection.Query<Employeeinfo>(sQuery, new { Id = id }).FirstOrDefault();
+            }
+            }
+        public void UpdateEmp(Employeeinfo Emp)
+        {
+            using (IDbConnection dbConnection = connection)
+            {
+                string sQuery = @"UPDATE Emptable SET EmpName=@EmpName,Department=@Department,Salary=@Salary WHERE EmpId=@EmpId";
+                dbConnection.Open();
+                 dbConnection.Query(sQuery, Emp) ;
+            }
+        }
+        public void DeleteEmp(int id)
+        {
+            using (IDbConnection dbConnection = connection)
+            {
+                string sQuery = @"DELETE FROM Emptable WHERE EmpId=@id";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, new { Id = id });
+            }
+        }
+    }
+   
+
+
+            
+}
